d24a36efe03e6e69e1ed6675580bc00372e872c2
IJJA3141 <109812161+IJJA3141@users.noreply.github.com>
2022-10-17 20:19:08 +0200

    Delete public/js/class directory

--- public/js/class/rectagle.js
+++
@@ -1,9 +0,0 @@
-class rectagle {
-    constructor(width, height, left, top) {
-        this.width = width;
-        this.height = height;
-        this.left = left;
-        this.top = top;
-    }
-}
-export { rectagle };
