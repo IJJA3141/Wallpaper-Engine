28a86243d3cd5e03939c3ddc3e207f19e7f9b7e8
IJJA3141 <109812161+IJJA3141@users.noreply.github.com>
2022-12-02 23:18:35 +0100

    class

--- back-end/back-end.cpp
+++ back-end/back-end.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 #include "crow.h"
 #include <comdef.h>
@@ -7,20 +7,27 @@
 
 #pragma comment(lib, "wbemuuid.lib")
 
-IWbemLocator* pLoc = NULL;
-IWbemServices* pSvc = NULL;
-IEnumWbemClassObject* pEnumerator = NULL;
-IWbemClassObject* pclsObj = NULL;
+class WMIQuery
+{
+public:
+	WMIQuery();
+	~WMIQuery();
 
-int cpuLOAD = 0;
-bool cpuRES = false;
-bool initRES = false;
+	IWbemLocator* pLoc;
+	IWbemServices* pSvc;
+	IEnumWbemClassObject* pEnumerator;
+	IWbemClassObject* pclsObj;
+private:
 
-crow::SimpleApp app;
-crow::json::wvalue ConstRES = NULL;
-crow::json::wvalue refreshRES = NULL;
+};
 
-bool initWMI() {
+WMIQuery::WMIQuery()
+{
+	pLoc = NULL;
+	pSvc = NULL;
+	pEnumerator = NULL;
+	pclsObj = NULL;
+
 	HRESULT hres;
 	pLoc = NULL;
 	pSvc = NULL;
@@ -29,21 +36,21 @@ bool initWMI() {
 	hres = CoInitializeEx(0, COINIT_MULTITHREADED);
 	if (FAILED(hres)) {
 		std::cout << "Failed to initialize COM library. Error code = 0x" << std::hex << hres << std::endl;
-		return false;
+		return;
 	}
 
 	hres = CoInitializeSecurity(NULL, -1, NULL, NULL, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE, NULL);
 	if (FAILED(hres)) {
 		std::cout << "Failed to initialize security. Error code = 0x" << std::hex << hres << std::endl;
 		CoUninitialize();
-		return false;
+		return;
 	}
 
 	hres = CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID*)&pLoc);
 	if (FAILED(hres)) {
 		std::cout << "Failed to create IWbemLocator object." << " Err code = 0x" << std::hex << hres << std::endl;
 		CoUninitialize();
-		return false;
+		return;
 	}
 
 	hres = pLoc->ConnectServer(_bstr_t(L"ROOT\\CIMV2"), NULL, NULL, 0, NULL, 0, 0, &pSvc);
@@ -51,7 +58,7 @@ bool initWMI() {
 		std::cout << "Could not connect. Error code = 0x" << std::hex << hres << std::endl;
 		pLoc->Release();
 		CoUninitialize();
-		return false;
+		return;
 	}
 
 	std::cout << "Connected to ROOT\\CIMV2 WMI namespace" << std::endl;
@@ -62,48 +69,64 @@ bool initWMI() {
 		pSvc->Release();
 		pLoc->Release();
 		CoUninitialize();
-		return false;
+		return;
 	}
 
 	std::cout << "Query initialized" << std::endl;
-	return true;
 }
+
+WMIQuery::~WMIQuery()
+{
+	pSvc->Release();
+	pLoc->Release();
+	CoUninitialize();
+}
+
+WMIQuery q = WMIQuery();
+
+int cpuLOAD = 0;
+bool cpuRES = false;
+bool initRES = false;
+
+crow::SimpleApp app;
+crow::json::wvalue ConstRES = NULL;
+crow::json::wvalue refreshRES = NULL;
 
 bool getCPUdata(int* ptrL, int* ptrT) {
 	HRESULT hres;
 	ULONG uReturn = 0;
 	VARIANT vtProp;
-	pEnumerator = NULL;
+	q.pEnumerator = NULL;
 	BSTR tempBSTR = NULL;
 
-	hres = pSvc->ExecQuery(bstr_t("WQL"), bstr_t("SELECT * FROM Win32_PerfRawData_PerfOS_Processor where Name='_Total' "), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &pEnumerator);
+	hres = q.pSvc->ExecQuery(bstr_t("WQL"), bstr_t("SELECT * FROM Win32_PerfRawData_PerfOS_Processor where Name='_Total' "), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &q.pEnumerator);
 	if (FAILED(hres)) {
 		std::cout << "Query for operating system name failed." << std::hex << hres << std::endl;
-		pSvc->Release();
-		pLoc->Release();
+		q.pSvc->Release();
+		q.pLoc->Release();
 		CoUninitialize();
 		return false;
 	}
 	
-	pclsObj = NULL;
+	q.pclsObj = NULL;
 
-	while (pEnumerator) {
-		hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+	while (q.pEnumerator) {
+		hres = q.pEnumerator->Next(WBEM_INFINITE, 1, &q.pclsObj, &uReturn);
 		if (uReturn == 0) break;
 
-		hres = pclsObj->Get(L"PercentProcessorTime", 0, &vtProp, 0, 0);
+		hres = q.pclsObj->Get(L"PercentProcessorTime", 0, &vtProp, 0, 0);
 		tempBSTR = (vtProp.bstrVal);
 		*ptrL = _wtoi64(tempBSTR);
 
 		VariantClear(&vtProp);
 
-		hres = pclsObj->Get(L"TimeStamp_Sys100NS", 0, &vtProp, 0, 0);
+		hres = q.pclsObj->Get(L"TimeStamp_Sys100NS", 0, &vtProp, 0, 0);
 		tempBSTR = (vtProp.bstrVal);
 		*ptrT = _wtoi64(tempBSTR);
 
 		VariantClear(&vtProp);
-		pclsObj->Release();
-		pclsObj = NULL;
+		q.pclsObj->Release();
+		q.pclsObj = NULL;
 	}
 
 	return true;
@@ -117,16 +140,16 @@ bool getCPUload(int* ptrO){
 	float res = 0.0;
 
 	if (getCPUdata(&l1, &t1)) {
-		Sleep(2000);
+		Sleep(1000);
 		
 		if (getCPUdata(&l2, &t2)) {
 			if (l2 == l1 || t2 == t1) {
 				return false;
 			}
-			
-			res = (1 - (static_cast<float>(l2) - l1) / (t2 - t1)) * 100 * 100;
+			std::wcout << L"DL:" << l2 - l1 << L"\nDT:" << t2 - t1 << std::endl;
+			res = (1 - (static_cast<float>(l2) - l1) / (t2 - t1)) * 100;
 			*ptrO = res;
-			std::cout << *ptrO << "%%" << std::endl;
+			std::cout << *ptrO << "%" << std::endl;
 
 			return true;
 		}
@@ -152,19 +175,10 @@ void serverSetUp() {
 }
 
 int main() {
-	initWMI();
 	getCPUload(&cpuLOAD);
 	
-	/*serverSetUp();
-	app.port(18080).run();*/
+	serverSetUp();
+	app.port(18080).run();
 
 	return 0;
 }
-
-/*
-
-NOTE A MOI MEME:
-PercentProcessorTime NE DONE PAS LE % DU CPU -->
-SUIVRE LA FORMULE DE STACKOVERFLOWw
-
-*/
