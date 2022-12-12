c1760ab42f484cb75f1b651a25369d1861abc443
IJJA3141 <109812161+IJJA3141@users.noreply.github.com>
2022-12-12 21:50:11 +0100

    graf

--- rngtest/index.js
+++ rngtest/index.js
@@ -1,25 +1,11 @@
 import fetch from "node-fetch";
 
-const obj = "ligne1//n//ligne2//n//sfdsaaek//n//fkadeal";
-
-fetch("http://localhost:18080/write", {
-    method: "POST",
-    body: JSON.stringify(obj)
-}
-)
-
 function c(){
     console.log(">")
     fetch("http://localhost:18080/refresh");
     console.log("||")
 }
 
-fetch("http://localhost:18080/getConst");
-
-function f(){
+for(let i = 0; i < 500; i++){
     setTimeout(c, 5000)
-}
-
-for(let i = 0; i < 10000; i++){
-    f();
 }
